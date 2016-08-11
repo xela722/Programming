c865546ba6e10d058dc4efbcef9c488ef67ec39f
xela722 <alex0722@comcast.net>
2016-08-11 11:51:51 -0400

    New player will add to table

--- Baseball/script.js
+++ Baseball/script.js
@@ -2,14 +2,13 @@
 
 var players = [];
 var x = 0;
-
-var y = 0;
 var row = [];
-var z = 0;
 var cell = [];
+var y = 0;
+var z = 0;
 var w = 0;
 function newPlayer() {
-    var tab = document.getElementById('Tbl');
+
     var tempNum = document.getElementById('num').value;
     var tempName = document.getElementById('name').value;
     var tempAb = document.getElementById('ab').value;
@@ -21,16 +20,13 @@ function newPlayer() {
         tempAb = null;
         tempH = null;
         console.log(players[x]);
+        addData();
         x++;
+    }else {
+        window.alert("Too many players!");
     }
-    if (y<12) {
-        row[y] = tab.insertRow(y+1);
-        if (z<4) {
-            cell[z] = row[y].insertCell(0);
-            z++;
-        }
-        y++;
-    }
+
+
 
 };
 function player(num, name, ab, h){
@@ -40,3 +36,24 @@ function player(num, name, ab, h){
     this.h = h;
     this.avg = this.h/this.ab;
 };
+function addData(){
+    var tab = document.getElementById('Tbl');
+    row[y] = tab.insertRow(y+1);
+    for (var i = 0; i <5 ; i++) {
+
+        cell[i] = row[y].insertCell(i);
+        if (i === 0) {
+            cell[i].innerHTML = players[x].num;
+        } else if (i === 1) {
+            cell[i].innerHTML = players[x].name;
+        } else if (i === 2) {
+            cell[i].innerHTML = players[x].ab;
+        } else if (i === 3) {
+            cell[i].innerHTML = players[x].h
+        } else if (i === 4) {
+            cell[i].innerHTML = players[x].avg;
+            y++
+        }
+    }
+
+}
